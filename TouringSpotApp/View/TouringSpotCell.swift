b620cdb79ba507b43d6146a72b6a83b339e6923f
LittIe-ma <yasudaM84@gmail.com>
2022-03-06 22:34:50 +0900

    Outlet接続

--- TouringSpotApp/View/TouringSpotCell.swift
+++ TouringSpotApp/View/TouringSpotCell.swift
@@ -10,4 +10,14 @@ import UIKit
 final class TouringSpotCell: UITableViewCell {
 
     static var className: String { String(describing: TouringSpotCell.self) }
+
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var addressLabel: UILabel!
+    @IBOutlet private weak var spotImage: UIImageView!
+
+    func configure(touringSpotData: TouringSpotData) {
+        self.nameLabel.text = touringSpotData.name
+        self.addressLabel.text = touringSpotData.address
+        self.spotImage.image = touringSpotData.image
+    }
 }
