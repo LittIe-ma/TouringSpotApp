051d803d0f101028b810c2f5aab66660029d6aeb
LittIe-ma <yasudaM84@gmail.com>
2022-03-06 22:34:59 +0900

    Model作成

---
+++ TouringSpotApp/Model/TouringSpotData.swift
@@ -0,0 +1,15 @@
+//
+//  TouringSpotData.swift
+//  TouringSpotApp
+//
+//  Created by Masato Yasuda on 2022/03/06.
+//
+
+import Foundation
+import UIKit
+
+struct TouringSpotData {
+    let name: String
+    let address: String
+    let image: UIImage
+}
