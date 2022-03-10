e84440f2f00c079c0e36a5ea3a1097a7d0dde5e1
LittIe-ma <yasudaM84@gmail.com>
2022-03-10 15:49:25 +0900

    csvからデータを取得するメソッド作成

--- TouringSpotApp/Model/TouringSpotModel.swift
+++ TouringSpotApp/Model/TouringSpotModel.swift
@@ -5,11 +5,37 @@
 //  Created by Masato Yasuda on 2022/03/06.
 //
 
-import Foundation
 import UIKit
 
 struct TouringSpotModel {
     let name: String
     let address: String
-    let image: UIImage
+//    let imageStr: String
+}
+
+final class loadCSV {
+
+    static let shared: loadCSV = .init()
+    private init() {}
+
+    func get(completion: ((Result<TouringSpotModel, Error>) -> Void)? = nil) {
+
+        guard let path = Bundle.main.path(forResource:"TouringSpotData", ofType:"csv") else { return }
+
+        var csvLines: [String]
+
+        do {
+            let csvString = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
+            csvLines = csvString.components(separatedBy: .newlines)
+            csvLines.removeLast()
+        } catch let error {
+            completion?(.failure(error))
+            return
+        }
+
+        for data in csvLines {
+            let detail = data.components(separatedBy: ",")
+            completion?(.success(TouringSpotModel(name: detail[0], address: detail[1])))
+        }
+    }
 }
