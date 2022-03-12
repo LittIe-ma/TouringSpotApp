b66d3449785b8f0f5f1b9af6f911dd03efc0bd52
LittIe-ma <yasudaM84@gmail.com>
2022-03-12 17:37:32 +0900

    ファイル追加

---
+++ TouringSpotApp/Controller/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  TouringSpotApp
+//
+//  Created by Masato Yasuda on 2022/03/10.
+//
+
+import UIKit
+import MapKit
+
+final class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
